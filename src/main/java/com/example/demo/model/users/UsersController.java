e98cf1b89767097f3a18dad1851beadaec34c454
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2022-03-17 01:22:57 +0700

    deleteUser (DELETE)

--- src/main/java/com/example/demo/model/users/UsersController.java
+++ src/main/java/com/example/demo/model/users/UsersController.java
@@ -1,18 +1,22 @@
 package com.example.demo.model.users;
 
+import com.example.demo.model.data.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path="api/v1/users")
 public class UsersController
 {
     private final UserService userService;
+    private final DataService dataService;
     @Autowired
-    public UsersController(UserService userService) {
+    public UsersController(UserService userService, DataService dataService) {
         this.userService = userService;
+        this.dataService = dataService;
     }
 
     @GetMapping
@@ -24,7 +28,13 @@ public class UsersController
     @PostMapping("register")
     void registerNewUser(@RequestBody Login login)
     {
-
         userService.addUser(login);
+    }
+
+    @DeleteMapping(path="{username}")
+    void deleteUser(@PathVariable("username") String username)
+    {
+        UUID uuid = userService.deleteUser(username);
+        dataService.deleteData(uuid);
     }
 }
