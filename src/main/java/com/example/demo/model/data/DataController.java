eeefa67e40c5804f87f204b5f6a7c61b6d662ec1
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2022-03-17 17:00:02 +0700

    ??? maybe ide do this and fixed a little bug

--- src/main/java/com/example/demo/model/data/DataController.java
+++ src/main/java/com/example/demo/model/data/DataController.java
@@ -1,15 +1,11 @@
 package com.example.demo.model.data;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Repository
+@RestController
 @RequestMapping(path="api/v1/data")
 public class DataController {
 
