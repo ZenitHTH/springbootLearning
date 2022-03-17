a4da2dba76bc17644129fa68e57c68c053339f01
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2022-03-17 17:03:30 +0700

    fixed bug can't insert data and check username and duplicate bio

--- src/main/java/com/example/demo/model/data/DataService.java
+++ src/main/java/com/example/demo/model/data/DataService.java
@@ -1,14 +1,15 @@
 package com.example.demo.model.data;
 
-import com.example.demo.model.users.User;
 import com.example.demo.model.users.UserRepository;
 import com.example.demo.model.users.UserService;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -16,6 +17,8 @@ public class DataService {
     private final DataRepository dataRepository;
     private final UserRepository userRepository;
     private final UserService userService;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     public DataService(DataRepository dataRepository, UserRepository userRepository, UserService userService) {
@@ -30,27 +33,45 @@ public class DataService {
 
     public void deleteData(@NotNull UUID uuid)
     {
-        Optional<Data> data = dataRepository.findDatabyUUID(uuid);
-        boolean exist = dataRepository.existsById(data.get().getId());
+        Data data = findDatabyUUID(uuid).get(0);
+        boolean exist = dataRepository.existsById(data.getId());
         if(!exist)
         {
             throw new IllegalStateException("Cannot find Data");
         }
 
-        dataRepository.delete(data.get());
+        dataRepository.delete(data);
     }
 
-    public void insertData(PostData postData) throws IllegalStateException
+    public void insertData(@NotNull PostData postData) throws IllegalStateException
     {
-        UUID uuid = userService.findUserbyUsername(postData.getUsername()).get(0).getUuid();
-        Optional<Data> data = dataRepository.findDatabyUUID(uuid);
-        if(data.get().getUuid().equals(uuid))
+        UUID uuid;
+        try {
+            uuid = userService.findUserbyUsername(postData.getUsername()).get(0).getUuid();
+
+        }catch (Exception e)
         {
-            throw new IllegalStateException("User "+ postData.getUsername() +"Bio has duplicate");
+            throw new IllegalStateException("Not Have this username");
+        }
+        if(uuid != null)
+        {
+            if(findDatabyUUID(uuid).size() > 0)
+            {
+                throw new IllegalStateException("User "+ postData.getUsername() +" Bio has duplicate");
+            }
+
+            List<String> bd = Arrays.asList(postData.getBirthday().split("-"));
+            LocalDate birthday = LocalDate.of(new Integer(bd.get(2)),new Integer(bd.get(1)),new Integer(bd.get(0)));
+            Data _data = new Data(uuid,postData.getUsername(),birthday,postData.getBio());
+            dataRepository.save(_data);
         }
 
-        dataRepository.save(new Data(uuid,postData.getUsername(),postData.getBirthday(),postData.getBio()));
+    }
 
+    @Transactional
+    public List<Data> findDatabyUUID(UUID uuid)
+    {
+        return jdbcTemplate.query("SELECT * FROM data_table d WHERE d.UUID='"+uuid.toString()+"'",new DataRowMapper());
     }
 
 }
